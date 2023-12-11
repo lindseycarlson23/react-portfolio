520cd14f2888ffd29836064ec2fe36ce2f5f72e0
lindseycarlson23 <144490541+lindseycarlson23@users.noreply.github.com>
2023-12-11 10:57:35 -0600

    added fitness tracker info

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -15,6 +15,14 @@ const projects = [
         alt: "image of man lifting weights at a gym",
         gitHubUrl: "https://github.com/lindseycarlson23/my-workouts",
         deployedUrl: "https://my-workouts-c1e5314d5b06.herokuapp.com/"
+    },
+    {
+        key: 3,
+        title: "Fitness Tracker",
+        image: "/images/fitnesstrackerscreenshothomepage.png",
+        alt: "blue background and white text that says Fitness Tracker with image of heart and notepad",
+        gitHubUrl: "https://github.com/Bfinch3/fitness-tracker",
+        deployedUrl: "https://fitoness-tracker-779b260a5616.herokuapp.com/"
     }
 ]
 
